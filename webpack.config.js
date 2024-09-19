420b9f8849c8821b494b953806360b3dca82acc5
Donghyeon Lee <ehdgus001928@gmail.com>
2024-09-20 00:31:16 +0900

    conf: 배포 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,71 +2,75 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'auto',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.css'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                namedExport: false,
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: isProduction ? '/page/template/' : '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.css'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  namedExport: false,
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-    ],
-    compress: false,
-    port: 3005,
-    historyApiFallback: true,
-    proxy: [
-      {
-        context: ['/pool'],
-        target: 'http://34.47.117.26',
-      },
-      {
-        context: ['/template'],
-        target: 'http://34.47.117.26',
-      },
-    ],
-  },
+    devServer: {
+      static: [
+        {
+          directory: path.join(__dirname, 'dist'),
+        },
+      ],
+      compress: false,
+      port: 3005,
+      historyApiFallback: true,
+      proxy: [
+        {
+          context: ['/pool'],
+          target: 'http://34.47.117.26',
+        },
+        {
+          context: ['/template'],
+          target: 'http://34.47.117.26',
+        },
+      ],
+    },
+  };
 };
